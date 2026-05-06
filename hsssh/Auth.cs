370e0964b3f2308aebab317432ffa90c50a31cbd
httpsColon <102579986+httpsColon@users.noreply.github.com>
2026-05-06 22:27:12 +0800

    Update Auth.cs

--- hsssh/Auth.cs
+++ hsssh/Auth.cs
@@ -78,9 +78,8 @@ namespace HSssh
         {
             string cfg = Directory.GetCurrentDirectory() + @"\hsssh.cfg";
             string cid = GetCIdViaCommand();
-            return cid == "BFEBFBFF000B06A2";
 
-            //return cid == RC4Helper.Decrypt(File.ReadAllBytes(cfg), "hsssh") || cid == RC4Helper.Decrypt2(File.ReadAllBytes(cfg), "hsssh");
+            return cid == RC4Helper.Decrypt(File.ReadAllBytes(cfg), "hsssh") || cid == RC4Helper.Decrypt2(File.ReadAllBytes(cfg), "hsssh");
 
         }
 
