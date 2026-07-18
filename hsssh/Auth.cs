8392a35349094fa235ab14bd4305e96fa6dedd29
httpsColon <102579986+httpsColon@users.noreply.github.com>
2026-07-18 19:50:35 +0800

    Update Auth.cs

--- hsssh/Auth.cs
+++ hsssh/Auth.cs
@@ -68,7 +68,7 @@ namespace HSssh
             }
             catch
             {
-                return "wmic Error";
+                return GetCIdViaPowerShell();
             }
         }
         
@@ -78,9 +78,8 @@ namespace HSssh
         {
             string cfg = Directory.GetCurrentDirectory() + @"\hsssh.cfg";
             string cid = GetCIdViaCommand();
-            return GetBId()  =="503323263";
 
-            //return cid == RC4Helper.Decrypt(File.ReadAllBytes(cfg), "hsssh") || cid == RC4Helper.Decrypt2(File.ReadAllBytes(cfg), "hsssh");
+            return cid == RC4Helper.Decrypt(File.ReadAllBytes(cfg), "hsssh") || cid == RC4Helper.Decrypt2(File.ReadAllBytes(cfg), "hsssh");
 
         }
 
